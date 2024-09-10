a2ccb1961a3f4da4171f62d6d2635879bbaa10cc
Haritha <haritha.zenova@gmail.com>
2024-09-10 21:48:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./Scenes/Login";
+import { Route, Routes } from "react-router-dom";
+import Test from "./Scenes/Test";
+import HomeScreen from "./Scenes/HomeScreen";
+import ReservationPage from "./Scenes/Reservation";
+import StaffDashboard from "./Scenes/StaffDashboard";
+import CustomerRegistrationPage from "./Scenes/CustomerRegistration";
+import StaffRegistrationPage from "./Scenes/StaffRegistration";
+import LoginPage from "./Scenes/Login";
+import StaffLoginPage from "./Scenes/StaffLogin";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route index path="/" element={<HomeScreen />} />
+        <Route index path="/login" element={<LoginPage />} />
+        <Route index path="/reservation" element={<ReservationPage />} />
+        <Route index path="/customer-register" element={<CustomerRegistrationPage />} />
+        <Route index path="/staff-register" element={<StaffRegistrationPage />} />
+        <Route index path="/staff-login" element={<StaffLoginPage />} />
+        <Route index path="/staff-dashboard" element={<StaffDashboard />} />
+      </Routes>
     </div>
   );
 }
